fe6c3c301e9c98a9ced6dccac5ed5b45d2c20110
relow <dev.antropov@gmail.com>
2024-09-27 16:24:20 +0300

    work progress

--- examples/complicated-shader/index.js
+++ examples/complicated-shader/index.js
@@ -1,8 +1,3 @@
-import { createShader, createProgram, vertexShaderSource, loadTexture } from "../../baseCode/shaders.js";
-
-const canvas = document.querySelector("canvas");
-const gl = canvas.getContext('webgl');
-
 const fragmentShaderSource = `
 precision mediump float;
 uniform vec2 iResolution;
@@ -14,33 +9,29 @@ float isFilled(vec2 st) {
     return sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123;
 }
 
-void main() {
-    // float cellSize = 64.0;
-    // vec2 st = gl_FragCoord.xy / cellSize;
-    // vec2 nc = fract(st);
-    // vec2 np = floor(mod(gl_FragCoord.xy, cellSize));
-    // vec2 idx = floor(st);
-    // float isPx = texture2D(iChannel0, vec2(0.5,0.) + (nc / 8.)).r;
-    //
-    // // vec3 color = vec3(step(0.5, fract(isFilled(idx))));
-    // //
-    // // color *= 1.0 - smoothstep(0.0, 200.0, length(gl_FragCoord.xy - iMouse.xy)) * 0.6;
-    //
-    // if (isPx > 0.5) {
-    //   gl_FragColor = vec4(0., 1., 1., isPx);
-    // } else {
-    //   gl_FragColor = vec4(0.,0.,0., 1.0);
-    // }
-    
-    vec2 uv = vec2(gl_FragCoord.x, iResolution.y - gl_FragCoord.y);
-    
-    if (uv.x > 512. || uv.y > 512.) {
-      gl_FragColor = vec4(0.);
-    } else {
-      gl_FragColor = texture2D(iChannel0, uv / 512.);
-    }
+float rand(vec2 coord) {
+    return fract(sin(dot(coord, vec2(12.9898, 78.233))) * 43758.5453123) < 0.5 ? 0.0 : 0.5;
 }
-`;
+
+void main() {
+    vec2 uv = vec2(gl_FragCoord.x - iResolution.x / 2., iResolution.y / 2. - gl_FragCoord.y); // px
+    vec2 st = uv / 16.;
+    vec2 nc = fract(st) / 2.;
+    vec2 idx = floor(st);
+    nc.x += rand(idx);
+    float color1 = texture2D(iChannel0, vec2(nc)).r;
+    float color2 = texture2D(iChannel0, vec2(nc.x, nc.y + 0.5)).r;
+    if (color1 != 0.) {
+      gl_FragColor = vec4(1.) * color1;
+    } else {
+      // gl_FragColor = vec4(1.);
+    }
+}`;
+
+import { createShader, createProgram, vertexShaderSource, loadTexture } from "../../baseCode/shaders.js";
+
+const canvas = document.querySelector("canvas");
+const gl = canvas.getContext('webgl');
 
 const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
 const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
@@ -70,7 +61,7 @@ gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
 gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-const texture0 = loadTexture('texture.png', gl)
+const texture0 = loadTexture('letters-texture3.png', gl)
 
 function resizeCanvasToDisplaySize(canvas) {
   const displayWidth = canvas.clientWidth;
